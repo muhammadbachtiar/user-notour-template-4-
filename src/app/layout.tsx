8ab09dc4ed79ffc11903ebcbc623aec0f010a8a1
Muhammad Bachtiar <muhammadbachtiar010902@gmail.com>
2025-07-04 14:21:55 +0700

    Fix SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClientWrapper>
-          <div className="min-h-screen min-w-full bg-primary flex flex-col justify-between items-start w-full overflow-x-hidden">
+          <div className="min-h-screen min-w-full bg-primary flex flex-col justify-between items-start w-full">
                 <div className="absolute top-0 left-0 w-full z-50 ">
                   <Header />
                 </div>
